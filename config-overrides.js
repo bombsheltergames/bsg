f2a9718173ff95476630116bafe070b53efea195
Steve Leach <sleach@wayfair.com>
2018-10-10 12:31:43 -0400

    Updates

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-/* config-overrides.js */
-const rewireStyledComponents = require("react-app-rewire-styled-components");
-
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  config = rewireStyledComponents(config, env);
-  return config;
-};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "styles/index.css";
-import Main from "./main";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<Main />, document.getElementById("main-root"));
-registerServiceWorker();
