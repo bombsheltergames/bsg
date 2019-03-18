dc5b623336b8ab1bc17bba407a87c7ddeef4b01c
Steve Leach <sleach@wayfair.com>
2019-03-18 10:55:25 -0400

    unregister serviceworker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "styles/index.css";
 import Main from "./main";
-import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(<Main />, document.getElementById("main-root"));
-registerServiceWorker();
+unregister();
