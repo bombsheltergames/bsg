ac6ca9536a35bf5b3668b3d3c28937c392a469b7
Steve Leach <sleach@wayfair.com>
2019-06-06 10:50:35 -0400

    Updates

---
+++ docs/precache-manifest.e64996e96719202e4dbe68896be7f372.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090594ec8c6f7483dbfaefab70c3cdbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "461d7b70edc6e0a1edbf",
+    "url": "/static/css/main.c657df98.chunk.css"
+  },
+  {
+    "revision": "b336e180f066d17e30eb",
+    "url": "/static/js/2.5e1ee62f.chunk.js"
+  },
+  {
+    "revision": "461d7b70edc6e0a1edbf",
+    "url": "/static/js/main.8441e494.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d23a45d6fc41dd7ce8d246bc2958a56d",
+    "url": "/static/media/01.d23a45d6.jpg"
+  },
+  {
+    "revision": "bcff39b23176bbf4373b4ae1eda4f1d7",
+    "url": "/static/media/02.bcff39b2.jpg"
+  },
+  {
+    "revision": "b370b1345984065f711aeaa1e6d1930a",
+    "url": "/static/media/03.b370b134.jpg"
+  },
+  {
+    "revision": "b0836ea1ce7fbebe259fec0db885f247",
+    "url": "/static/media/04.b0836ea1.jpg"
+  },
+  {
+    "revision": "16ea5a13d3407b7a043c5fcfaaef567e",
+    "url": "/static/media/05.16ea5a13.jpg"
+  },
+  {
+    "revision": "ad96555c801dfd6c17064c93a30115a3",
+    "url": "/static/media/06.ad96555c.jpg"
+  },
+  {
+    "revision": "ed174c717f5d9ce125ea9a2613d85ada",
+    "url": "/static/media/07.ed174c71.jpg"
+  },
+  {
+    "revision": "da5e8d6d6da048408508210b61a3fb9c",
+    "url": "/static/media/08.da5e8d6d.jpg"
+  },
+  {
+    "revision": "f37a7a8fb61b8712d38102dd774575f9",
+    "url": "/static/media/09.f37a7a8f.jpg"
+  },
+  {
+    "revision": "e0aa48054d43b46a88c3f44c88006a50",
+    "url": "/static/media/10.e0aa4805.jpg"
+  },
+  {
+    "revision": "fc3f5bad6e465c20aaf8bbfa7e2c8f02",
+    "url": "/static/media/dos-hero.fc3f5bad.png"
+  },
+  {
+    "revision": "31c25b0be0ceb13364b346aeaf411c28",
+    "url": "/static/media/dos-previewImage.31c25b0b.png"
+  },
+  {
+    "revision": "d20e8fbe3629bc0ebf830eea09d3e58e",
+    "url": "/static/media/dos-tentacles-sm.d20e8fbe.png"
+  },
+  {
+    "revision": "f34300565156a13d745b13f5213bead6",
+    "url": "/static/media/dos-tentacles.f3430056.png"
+  },
+  {
+    "revision": "5049755aca60b459d0df0bab44735d71",
+    "url": "/static/media/page-background.5049755a.png"
+  },
+  {
+    "revision": "632304a43cfb1875d8b7c8fb6020c790",
+    "url": "/static/media/page-foreground.632304a4.png"
+  },
+  {
+    "revision": "7cdf700fbaee45aef05bb8c21250fd6a",
+    "url": "/static/media/play-btn.7cdf700f.svg"
+  },
+  {
+    "revision": "464f3388f10849aef4a75dba6ee9150f",
+    "url": "/static/media/sonar.464f3388.gif"
+  }
+]);
